de3e3e45608743e2b31f3c7124683c5d25ca8fcc
Marcos Martinez <mmtz2108@gmail.com>
2022-06-22 17:07:25 -0500

    trying to fix front end issues.

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState }from "react";
 import styled from "styled-components";
 
 const LoginPage = () => {
 
-const Container = styled.div
-  ` width: 100vw;
+const Container = styled.div`
+ width: 100vw;
   height: 100vh;
   background: linear-gradient(
       rgba(255, 255, 255, 0.5),
@@ -16,49 +15,48 @@ const Container = styled.div
   background-size: cover;
   display: flex;
   align-items: center;
-  justify-content: center; `
-;
+  justify-content: center;
+`;
 
-const Wrapper = styled.div
-` width: 25%;
+const Wrapper = styled.div` 
+width: 25%;
   padding: 20px;
-  background-color: white; `
+  background-color: white;
+`;
 
-;
+const Title = styled.h1`  
+font-size: 24px;
+  font-weight: 300;
+`;
 
-const Title = styled.h1
-`  font-size: 24px;
-  font-weight: 300; `
-;
+const Form = styled.form`
+ display: flex;
+  flex-direction: column; 
+`;
 
-const Form = styled.form
-` display: flex;
-  flex-direction: column; `
-;
-
-const Input = styled.input
-`  flex: 1;
+const Input = styled.input`
+  flex: 1;
   min-width: 40%;
   margin: 10px 0;
-  padding: 10px; `
-;
+  padding: 10px; 
+`;
 
-const Button = styled.button
-`  width: 40%;
+const Button = styled.button`
+  width: 40%;
   border: none;
   padding: 15px 20px;
   background-color: teal;
   color: white;
   cursor: pointer;
-  margin-bottom: 10px; `
-;
+  margin-bottom: 10px; 
+`;
 
-const Link = styled.a
-`  margin: 5px 0px;
+const Link = styled.a` 
+margin: 5px 0px;
   font-size: 12px;
   text-decoration: underline;
-  cursor: pointer; `
-;
+  cursor: pointer; 
+`;
     const [login, setLogin] = useState({
       username: '',
       password: '',
