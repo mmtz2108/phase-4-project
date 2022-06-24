b2ef1f64848dabce5e9dcf2774d3638b9c446d71
Marcos Martinez <mmtz2108@gmail.com>
2022-06-23 22:51:58 -0500

    added password protection

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :comments 
     has_many :posts, through: :comments
 
+    has_secure_password
+
     def authored_posts
         Post.where(poster_id: self.id) #covers has_many posts
     end
